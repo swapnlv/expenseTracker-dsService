4a3a722a35356a186455d2e3f2d50fe61658cc0c
swapnil <swapnilvishwakarma776@gmail.com>
2025-04-13 22:19:46 +0530

    Second Commit

--- src/main/java/com/service/openai/dsService/config/OpenAiConfig.java
+++ src/main/java/com/service/openai/dsService/config/OpenAiConfig.java
@@ -1,25 +1,45 @@
 package com.service.openai.dsService.config;
 
+import com.service.openai.dsService.service.SecretManagerService;
 import com.theokanning.openai.service.OpenAiService;
 import jakarta.annotation.PostConstruct;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenAiConfig {
-    @Value("${openai.ai.api.key}")
-    private String openApiKey;
+
+    private final SecretManagerService secretsManagerService;
+
+    @Value("${openai.api.key}")
+    private String openAiApiKey;
+
+
+    @Autowired
+    public OpenAiConfig(SecretManagerService secretsManagerService) {
+        this.secretsManagerService = secretsManagerService;
+        initApiKey();
+    }
+
+    @PostConstruct
+    private void initApiKey() {
+        String secretJson = secretsManagerService.getSecret("expenseTracker/openai");
+        JSONObject jsonObject = new JSONObject(secretJson);
+        this.openAiApiKey = jsonObject.getString("OPENAI_API_KEY");
+    }
 
     @Bean
     public OpenAiService openAiService() {
-        String apiKey = openApiKey;
-        System.out.println(apiKey);
+        String apiKey = openAiApiKey;
+//        System.out.println(apiKey);
         return new OpenAiService(apiKey);
     }
 
     @PostConstruct
     public void init() {
-        System.out.println("OpenAI API Key: " + openApiKey);
+        System.out.println("OpenAI API Key: " + openAiApiKey);
     }
 }
